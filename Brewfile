486a890d6be0f3326989cd79f2879fc4e5653de5
Jason Feng <jason@foxlime.io>
2020-07-09 23:36:37 +1000

    update

---
+++ Brewfile
@@ -0,0 +1,68 @@
+# taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+tap "homebrew/cask-fonts"
+tap "schniz/tap"
+
+# packages
+brew "ack"
+brew "bat"
+brew "cloc"
+brew "diff-so-fancy"
+brew "entr"
+brew "exa"
+brew "fd"
+brew "fzf"
+brew "gh"
+brew "git"
+brew "gnupg"
+brew "grep"
+brew "highlight"
+brew "htop"
+brew "hub"
+brew "jq"
+brew "lazydocker"
+brew "lazygit"
+brew "markdown"
+brew "mas"
+brew "nmap"
+brew "noti"
+brew "python"
+brew "reattach-to-user-namespace"
+brew "ripgrep"
+brew "schniz/tap/fnm"
+brew "shellcheck"
+brew "the_silver_searcher"
+brew "tig"
+brew "tldr"
+brew "tmux"
+brew "tree"
+brew "vim"
+brew "wdiff"
+brew "wget"
+brew "youtube-dl"
+brew "z"
+brew "zsh"
+
+# Applications
+cask "notion"
+cask "istat-menus"
+cask "paw"
+
+# Browsers
+cask "google-chrome"
+
+# Fonts
+cask "font-fira-code"
+cask "font-jetbrains-mono"
+cask "font-cascadia-mono"
+cask "font-3270-nerd-font"
+
+# macOS App Store
+mas "Drafts", id: 1435957248
+mas "Fantastical", id: 975937182
+mas "Xcode", id: 497799835
+mas "Reeder", id: 1449412482
+mas "Magnet", id: 441258766
+mas "Octotree Pro", id: 1457450145
--- install.sh
+++ install.sh
@@ -1,7 +1,54 @@
-echo "Symlinking dotfiles..."
-#Symlink .zshrc and .vimrc
+#!/usr/bin/env bash
+
+command_exists() {
+    type "$1" > /dev/null 2>&1
+}
+
+echo "Installing dotfiles..."
+
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/zshrc $HOME/.zshrc
 ln -s $PWD/tmux.conf $HOME/.tmux.conf
 ln -s $PWD/gitconfig $HOME/.gitconfig
 ln -s $PWD/hushlogin $HOME/hushlogin
+
+
+if test ! "$( command -v brew )"; then
+    echo "Installing homebrew"
+    ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+fi
+
+    # install brew dependencies from Brewfile
+    brew bundle
+
+    # After the install, setup fzf
+    echo -e "\\n\\nRunning fzf install script..."
+    echo "=============================="
+    /usr/local/opt/fzf/install --all --no-bash --no-fish
+
+    # Change the default shell to zsh
+    zsh_path="$( command -v zsh )"
+    if ! grep "$zsh_path" /etc/shells; then
+        echo "adding $zsh_path to /etc/shells"
+        echo "$zsh_path" | sudo tee -a /etc/shells
+    fi
+
+    if [[ "$SHELL" != "$zsh_path" ]]; then
+        chsh -s "$zsh_path"
+        echo "default shell changed to $zsh_path"
+    fi
+
+    source install/macos.sh
+
+    echo "creating vim directories"
+    mkdir -p ~/.vim-tmp
+
+    if ! command_exists zsh; then
+        echo "zsh not found. Please install and then re-run installation scripts"
+        exit 1
+    elif ! [[ $SHELL =~ .*zsh.* ]]; then
+        echo "Configuring zsh as default shell"
+        chsh -s "$(command -v zsh)"
+    fi
+
+    echo "Done. Reload your terminal."
