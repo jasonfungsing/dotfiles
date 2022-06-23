b45da347f93f6f15cb9d15bcf71d5d68444a9867
Jason Feng <jason@foxlime.io>
2022-06-23 16:20:12 +1000

    new url for download homebrew

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ ln -s $PWD/alias_prompt.sh $HOME/.alias_prompt.sh
 
 if test ! "$( command -v brew )"; then
     echo "Installing homebrew"
-    ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+    ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh )"
 fi
 
     # install brew dependencies from Brewfile
