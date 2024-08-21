5f77062c61661add72c932f1e95ba9a089cd7367
Jason Feng <jason@foxlime.io>
2024-08-22 09:11:44 +1000

    Update sublime.sh

--- sublime.sh
+++ sublime.sh
@@ -1,4 +1,4 @@
-# This is to link the sublime text config in dropbox
+# This is to link the sublime text config in icloud
 cd ~/Library/Application\ Support/Sublime\ Text/Packages
 rm -r User
 ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Apps/Sublime/User
